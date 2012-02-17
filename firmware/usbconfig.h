0c35464f831281a68fa77c96ed3e64eec105626e
Fabio Baltieri <fabio.baltieri@gmail.com>
2012-02-17 20:55:06 +0100

    usbconfig: change vendor/product strings

--- firmware/usbconfig.h
+++ firmware/usbconfig.h
@@ -242,8 +242,8 @@ section at the end of this file).
 #define USB_CFG_DEVICE_VERSION  0x00, 0x01
 /* Version number of the device: Minor number first, then major number.
  */
-#define USB_CFG_VENDOR_NAME     'o', 'b', 'd', 'e', 'v', '.', 'a', 't'
-#define USB_CFG_VENDOR_NAME_LEN 8
+#define USB_CFG_VENDOR_NAME     'w', 'w', 'w', '.', 'f', 'a', 'b', 'i', 'o', 'b', 'a', 'l', 't', 'i', 'e', 'r', 'i', '.', 'c', 'o', 'm'
+#define USB_CFG_VENDOR_NAME_LEN 21
 /* These two values define the vendor name returned by the USB device. The name
  * must be given as a list of characters under single quotes. The characters
  * are interpreted as Unicode (UTF-16) entities.
@@ -252,8 +252,8 @@ section at the end of this file).
  * obdev's free shared VID/PID pair. See the file USB-IDs-for-free.txt for
  * details.
  */
-#define USB_CFG_DEVICE_NAME     'T', 'e', 'm', 'p', 'l', 'a', 't', 'e'
-#define USB_CFG_DEVICE_NAME_LEN 8
+#define USB_CFG_DEVICE_NAME     'T', 'e', 's', 't', ' ', 'D', 'e', 'v', 'i', 'c', 'e'
+#define USB_CFG_DEVICE_NAME_LEN 11
 /* Same as above for the device name. If you don't want a device name, undefine
  * the macros. See the file USB-IDs-for-free.txt before you assign a name if
  * you use a shared VID/PID.
