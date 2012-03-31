5c756cca30198793bcc35471fd70ee3d5d343297
Fabio Baltieri <fabio.baltieri@gmail.com>
2012-03-31 19:02:47 +0200

    firmware: board.h: prepare macro for 6kW version

--- firmware/board.h
+++ firmware/board.h
@@ -23,4 +23,6 @@
 #define AMP_TH 45 /* ~ 1024 / 20 */
 #define SATURATION_TH 1020
 #define ADC_VREF_mV 2560
-#define V_TO_W(x) (x * 4000 / 2034) /* from schematics */
+
+#define V_TO_W(x) (x * 4000 / 2034) /* 4000W version */
+//#define V_TO_W(x) (x * 6500 / 2076) /* 6500W version */
