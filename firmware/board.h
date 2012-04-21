23987916f224ebd0b2d7151210dd448c4580b40d
Fabio Baltieri <fabio.baltieri@gmail.com>
2012-04-21 21:13:33 +0200

    firmware: change Vref, new values

--- firmware/board.h
+++ firmware/board.h
@@ -14,19 +14,19 @@
 
 /* ADC */
 
-#define ADC_COIL       (_BV(REFS2) | _BV(REFS1) | 0x02)
-#define ADC_OFFSET_1X  (_BV(REFS2) | _BV(REFS1) | 0x04)
-#define ADC_OFFSET_20X (_BV(REFS2) | _BV(REFS1) | 0x05)
-#define ADC_COIL_1X    (_BV(REFS2) | _BV(REFS1) | 0x06)
-#define ADC_COIL_20X   (_BV(REFS2) | _BV(REFS1) | 0x07)
+#define ADC_COIL       (_BV(REFS1) | 0x02)
+#define ADC_OFFSET_1X  (_BV(REFS1) | 0x04)
+#define ADC_OFFSET_20X (_BV(REFS1) | 0x05)
+#define ADC_COIL_1X    (_BV(REFS1) | 0x06)
+#define ADC_COIL_20X   (_BV(REFS1) | 0x07)
 
 #define AMP_TH 45 /* ~ 1024 / 20 */
-#define ADC_VREF_mV (2560 / 512)
-#define ADC_VREF_BITS (1024 / 512)
+#define ADC_VREF_mV (1100 / 4)
+#define ADC_VREF_BITS (1024 / 4)
 
 /* 4000W version */
 #define CAL_POWER	4000
-#define CAL_VOLTAGE	2034
+#define CAL_VOLTAGE	1121l
 /* 6500W version */
 //#define CAL_POWER	6500
-//#define CAL_VOLTAGE	2076
+//#define CAL_VOLTAGE	1060l
